d3a69d5e889d1465e53b966c1132c34e1b675125
Kseniia Lebedeva <lightgraywrite@gmail.com>
2023-04-03 13:02:20 -0700

    Init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { RegistrationComponent } from './login/registration/registration.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegistrationComponent
   ],
   imports: [
     BrowserModule

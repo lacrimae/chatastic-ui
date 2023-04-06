7d97762828e28fb1430f44fac4f6187bbf4b09dc
Kseniia Lebedeva <lightgraywrite@gmail.com>
2023-04-05 20:03:26 -0700

    Rewrote registration.component.html with reactive forms.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,35 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
+// import {MatSnackBarModule} from '@angular/material';
+import {ReactiveFormsModule} from '@angular/forms';
 
 import {AppComponent} from './app.component';
 import {RegistrationComponent} from './login/registration/registration.component';
 import {FormsModule} from "@angular/forms";
 import {LoginService} from "./service/LoginService";
 import {HttpClientModule} from "@angular/common/http";
+import {ErrorComponent} from './error/error.component';
+import {MatInputModule} from "@angular/material/input";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatCardModule} from "@angular/material/card";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
 @NgModule({
   declarations: [
     AppComponent,
-    RegistrationComponent
+    RegistrationComponent,
+    ErrorComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    // MatSnackBarModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatCardModule,
+    BrowserAnimationsModule
   ],
   providers: [LoginService],
   bootstrap: [AppComponent]
--- src/app/login/registration/registration.component.ts
+++ src/app/login/registration/registration.component.ts
@@ -1,5 +1,6 @@
 import {Component} from '@angular/core';
 import {LoginService, UserRequest} from "../../service/LoginService";
+import {FormControl, FormGroup, ValidationErrors, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-registration',
@@ -7,29 +8,57 @@ import {LoginService, UserRequest} from "../../service/LoginService";
   styleUrls: ['./registration.component.scss']
 })
 export class RegistrationComponent {
-  email: string = '';
-  nickname: string = '';
-  firstName: string = '';
-  lastName: string = '';
-  password: string = '';
+  registrationForm: FormGroup = new FormGroup({
+    email: new FormControl('', [Validators.email, Validators.required]),
+    nickname: new FormControl('', Validators.required),
+    firstName: new FormControl('', Validators.required),
+    lastName: new FormControl('', Validators.required),
+    password: new FormControl('', Validators.required)
+  });
 
   constructor(private api: LoginService) {
   }
 
-  /** Registers user. */
+  /** Registers a new user. */
   register() {
     const user: UserRequest = {
-      email: this.email,
-      nickname: this.nickname,
-      firstName: this.firstName,
-      lastName: this.lastName,
-      password: this.password
+      email: this.registrationForm.value.email,
+      nickname: this.registrationForm.value.nickname,
+      firstName: this.registrationForm.value.firstName,
+      lastName: this.registrationForm.value.lastName,
+      password: this.registrationForm.value.password
     };
 
-    this.api.register(user)
-      .subscribe(resp => {
-        console.log(resp);
-      });
+
+    this.api.register(user).subscribe({
+      next: (result) => {
+        // todo: Navigate to confirmation email page.
+        console.log(result)
+      },
+      error: (e) => {
+        // todo: error handling:  create a popup with error
+      },
+      complete: () => console.info('complete')
+    });
+  }
+
+  public hasSpecificError(formControlName: string, error: string): boolean {
+    const control = this.get(formControlName);
+    return control?.errors?.[error];
+  }
+
+  public hasError(formControlName: string): ValidationErrors | null {
+    const control = this.get(formControlName);
+    return control?.errors || null;
+  }
+
+  public isTouchedOrDirty(formControlName: string): boolean | undefined {
+    const control: FormControl<String> = this.get(formControlName);
+    return control?.touched || control?.dirty;
+  }
+
+  private get(formControlName: string): FormControl<String> {
+    return this.registrationForm.get(formControlName) as FormControl<String>;
   }
 }
 
